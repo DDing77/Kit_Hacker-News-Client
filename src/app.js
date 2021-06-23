36a1d381d36e78f6c9978d2149af76ca8cffecfd
DDing77 <smg76262585@gmail.com>
2021-06-23 21:18:32 +0900

    CH3-5

--- src/app.js
+++ src/app.js
@@ -11,35 +11,51 @@ function getData(url) {
     return JSON.parse(ajax.response);
 }
 
+function newsFeed() {
+    const newsFeed = getData(NEWS_URL);
+    const newsList = [];
 
-//JSON데이터를 객체로 바꾸기
-const newsFeed = getData(NEWS_URL);
-const ul = document.createElement('ul');
-
-window.addEventListener('hashchange', function() {
-   const id = location.hash.substr(1);
-
-   const newsContent = getData(CONTENT_URL);
-   const title = document.createElement('h1');
-
-   title.innerHTML = newsContent.title;
-
-   content.appendChild(title);
-});
-
-for(let i = 0; i < 10; i++) {
-    const div = document.createElement('div');
-
-    div.innerHTML = `
-    <li>
-        <a href="#${newsFeed[i].id}">
-        ${newsFeed[i].title} (${newsFeed[i].comments_count})
-        </a>
-    </li>
-    `;
-
-    ul.appendChild(div.firstElementChild);
+    newsList.push('<ul>');
+    
+    for(let i = 0; i < 10; i++) {
+        newsList.push(`
+        <li>
+            <a href="#${newsFeed[i].id}">
+            ${newsFeed[i].title} (${newsFeed[i].comments_count})
+            </a>
+        </li>
+        `);
+    }
+    
+    newsList.push('</ul>');
+    
+    container.innerHTML = newsList.join('');
 }
 
-container.appendChild(ul);
-container.appendChild(content);
+function newsDetail() {
+    const id = location.hash.substr(1);
+ 
+    const newsContent = getData(CONTENT_URL.replace('@id', id));
+ 
+    container.innerHTML = `
+     <h1>${newsContent.title}</h1>
+     
+     <div>
+         <a href="#">목록으로</a>
+     </div>
+     `;
+ }
+
+function router() {
+    const routePath = location.hash; // hash가 #만 반환하면 자동으로 아무값도 반환하지 않음
+
+    if (routePath === '') {
+        newsFeed();
+    } else {
+        newsDetail();
+    }
+}
+
+window.addEventListener('hashchange', router);
+
+router();
