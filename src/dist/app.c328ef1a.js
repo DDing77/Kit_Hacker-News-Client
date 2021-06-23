02a0e1d99d99f1404c3fffacf731defb20aa8bbe
DDing77 <smg76262585@gmail.com>
2021-06-23 20:49:44 +0900

    CH3-4

--- src/dist/app.c328ef1a.js
+++ src/dist/app.c328ef1a.js
@@ -123,29 +123,28 @@ var ajax = new XMLHttpRequest();
 var content = document.createElement('div');
 var NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 var CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
-ajax.open('GET', NEWS_URL, false);
-ajax.send(); //JSON데이터를 객체로 바꾸기
 
-var newsFeed = JSON.parse(ajax.response);
+function getData(url) {
+  ajax.open('GET', url, false);
+  ajax.send();
+  return JSON.parse(ajax.response);
+} //JSON데이터를 객체로 바꾸기
+
+
+var newsFeed = getData(NEWS_URL);
 var ul = document.createElement('ul');
 window.addEventListener('hashchange', function () {
   var id = location.hash.substr(1);
-  ajax.open('GET', CONTENT_URL.replace('@id', id), false);
-  ajax.send();
-  var newsContent = JSON.parse(ajax.response);
+  var newsContent = getData(CONTENT_URL);
   var title = document.createElement('h1');
   title.innerHTML = newsContent.title;
   content.appendChild(title);
 });
 
 for (var i = 0; i < 10; i++) {
-  var li = document.createElement('li');
-  var a = document.createElement('a');
-  a.href = "#".concat(newsFeed[i].id); //제목 + 댓글 표시
-
-  a.innerHTML = "".concat(newsFeed[i].title, " (").concat(newsFeed[i].comments_count, ")");
-  li.appendChild(a);
-  ul.appendChild(li);
+  var div = document.createElement('div');
+  div.innerHTML = "\n    <li>\n        <a href=\"#".concat(newsFeed[i].id, "\">\n        ").concat(newsFeed[i].title, " (").concat(newsFeed[i].comments_count, ")\n        </a>\n    </li>\n    ");
+  ul.appendChild(div.firstElementChild);
 }
 
 container.appendChild(ul);
