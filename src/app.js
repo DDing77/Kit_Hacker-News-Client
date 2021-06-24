052a7f0360daf7dab257ae186de162ab2346b62a
DDing77 <smg76262585@gmail.com>
2021-06-24 21:20:33 +0900

    CH3-6

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ const ajax = new XMLHttpRequest();
 const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
+const store = {
+    currentPage: 1,
+};
 
 function getData(url) {
     ajax.open('GET', url,false);
@@ -11,16 +14,17 @@ function getData(url) {
     return JSON.parse(ajax.response);
 }
 
+//글 목록
 function newsFeed() {
     const newsFeed = getData(NEWS_URL);
     const newsList = [];
 
     newsList.push('<ul>');
     
-    for(let i = 0; i < 10; i++) {
+    for(let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
         newsList.push(`
         <li>
-            <a href="#${newsFeed[i].id}">
+            <a href="#/show/${newsFeed[i].id}">
             ${newsFeed[i].title} (${newsFeed[i].comments_count})
             </a>
         </li>
@@ -28,12 +32,19 @@ function newsFeed() {
     }
     
     newsList.push('</ul>');
-    
+    newsList.push(`
+    <div>
+        <a href="#/page/${store.currentPage > 1 ? store.currentPage - 1 : 1}">이전 페이지</a>
+        <a href="#/page/${store.currentPage + 1}">다음 페이지</a>
+    </div>
+    `);
+
     container.innerHTML = newsList.join('');
 }
 
+//글 내용
 function newsDetail() {
-    const id = location.hash.substr(1);
+    const id = location.hash.substr(7);
  
     const newsContent = getData(CONTENT_URL.replace('@id', id));
  
@@ -41,15 +52,19 @@ function newsDetail() {
      <h1>${newsContent.title}</h1>
      
      <div>
-         <a href="#">목록으로</a>
+         <a href="#/page/${store.currentPage}">목록으로</a>
      </div>
      `;
  }
+
 
 function router() {
     const routePath = location.hash; // hash가 #만 반환하면 자동으로 아무값도 반환하지 않음
 
     if (routePath === '') {
+        newsFeed();
+    } else if (routePath.indexOf('#/page/') >= 0) {
+        store.currentPage = Number(routePath.substr(7));
         newsFeed();
     } else {
         newsDetail();
